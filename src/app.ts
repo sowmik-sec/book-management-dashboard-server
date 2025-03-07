ed0bbf1fff8c1477131eeab8356e65412cc6d79f
sowmik-sec <sowmik.sec@gmail.com>
2025-03-07 16:42:35 +0600

    add not found route

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,10 @@
-import express, { Express, Request, Response } from "express";
+import express, { Application, Request, Response } from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import router from "./app/routes";
 import globalErrorhandler from "./app/middlewares/globalErrorHandler";
-export const app: Express = express();
+import notFound from "./app/middlewares/notFound";
+const app: Application = express();
 
 app.use(express.json());
 app.use(cookieParser());
@@ -16,3 +17,7 @@ app.get("/", (req: Request, res: Response) => {
 });
 
 app.use(globalErrorhandler);
+
+app.use(notFound);
+
+export default app;
